8694fce8c71ea5484235dbb7911c1b963ef138dc
Wilson <wilsonadjei67200@gmail.com>
2025-06-09 20:56:19 +0200

    fix bug de duplication dans tableau de bord

--- caserneWAVZ/caserneWAVZ/mesDatas.cs
+++ caserneWAVZ/caserneWAVZ/mesDatas.cs
@@ -13,27 +13,33 @@ namespace caserneWAVZ
     private static DataSet dsGlobal = new DataSet();
     public static DataSet DsGlobal { get { return MesDatas.dsGlobal; } }
 
-        public static void FillDs()
+public static void FillDs()
+{
+    DataTable schemaTable = Connexion.Connec.GetSchema("Tables");
+    
+    for (int i = 0; i < schemaTable.Rows.Count; i++)
+    {
+        string nomTable = schemaTable.Rows[i][2].ToString();
+        string req = "select * from " + nomTable;
+        SQLiteCommand cd = new SQLiteCommand(req, Connexion.Connec);
+        SQLiteDataAdapter da = new SQLiteDataAdapter(cd);
+        da.Fill(dsGlobal, nomTable);
+
+        if (nomTable == "Mission")
         {
-            DataTable schemaTable = Connexion.Connec.GetSchema("Tables");
-            
-            for (int i = 0; i < schemaTable.Rows.Count; i++)
-            {
-                string nomTable = schemaTable.Rows[i][2].ToString();
-                string req = "select * from " + nomTable;
-                SQLiteCommand cd = new SQLiteCommand(req, Connexion.Connec);
-                SQLiteDataAdapter da = new SQLiteDataAdapter(cd);
-                da.Fill(dsGlobal, nomTable);
-            }
-
-            dsGlobal.Relations.Add("Mission_Caserne",
-            dsGlobal.Tables["Caserne"].Columns["id"],
-            dsGlobal.Tables["Mission"].Columns["idCaserne"]);
-
-            dsGlobal.Relations.Add("Mission_Sinistre",
-            dsGlobal.Tables["NatureSinistre"].Columns["id"],
-            dsGlobal.Tables["Mission"].Columns["idNatureSinistre"]);
+            dsGlobal.Tables["Mission"].PrimaryKey = new DataColumn[] { dsGlobal.Tables["Mission"].Columns["id"] };
         }
+    }
+
+    dsGlobal.Relations.Add("Mission_Caserne",
+        dsGlobal.Tables["Caserne"].Columns["id"],
+        dsGlobal.Tables["Mission"].Columns["idCaserne"]);
+
+    dsGlobal.Relations.Add("Mission_Sinistre",
+        dsGlobal.Tables["NatureSinistre"].Columns["id"],
+        dsGlobal.Tables["Mission"].Columns["idNatureSinistre"]);
+}
+
 }
 
 }
